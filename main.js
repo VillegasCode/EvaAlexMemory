ab619e67a6ddfe9a8a91fb3bb54038479ba14825
Carlos Villegas <carlos.villegasch@gmail.com>
2022-12-16 12:25:54 -0500

    creating variables to save values and compare tales with each other

--- main.js
+++ main.js
@@ -10,7 +10,18 @@ let timerInicial = 30;
 let temporizador = false;
 let tiempoRegresivoId = null;
 
+//Creating variables to ACIERTOS and MOVIMIENTOS
+let aciertos = 0;
+let movimientos = 0;
 
+//Creating variables to compare first result of a tale with second result fo a tale
+let primerResultado = null;
+let segundoResultado = null;
+
+//Creating variables to save the value of the tales
+let tarjeta1 = null;
+let tarjeta2 = null;
+let tarjetasDestapadas = 0;
 
 //Create un array with duplicate numbers of total of tales
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];

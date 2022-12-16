8ac7955385e1861972642c086fcd461a16ee80ed
Carlos Villegas <carlos.villegasch@gmail.com>
2022-12-16 12:19:11 -0500

    Creating variables to count time and hits and movements

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ let mostrarAciertos = document.getElementById("aciertos");
 let mostrarTiempo = document.getElementById("t-restante");
 let ocultarBoton = document.getElementById("reiniciar").style.visibility = 'hidden';
 
+//Creating variables to count time
+let timer = 30;
+let timerInicial = 30;
+let temporizador = false;
+let tiempoRegresivoId = null;
+
+
+
 //Create un array with duplicate numbers of total of tales
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];
 
@@ -12,3 +20,10 @@ numeros = numeros.sort(()=>{return Math.random()-0.5});
 //Write in console the numeros array of random way
 console.log(numeros);
 
+//Function to count time
+function contarTiempo() {
+    tiempoRegresivoId = setInterval(()=>{
+        timer--;
+        mostrarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
+    })
+}

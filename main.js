619372b1296655254bd333e66c7e9dfcac402e40
Carlos Villegas <carlos.villegasch@gmail.com>
2022-12-16 12:03:16 -0500

    Pointing to IDs into index.html and creating an array with random numbers

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+//Pointing to html document to its IDs with quatotion marks
+let mostrarMovimientos = document.getElementById("movimientos");
+let mostrarAciertos = document.getElementById("aciertos");
+let mostrarTiempo = document.getElementById("t-restante");
 let ocultarBoton = document.getElementById("reiniciar").style.visibility = 'hidden';
+
+//Create un array with duplicate numbers of total of tales
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];
+
+//We generate random numbers with arrow function
+numeros = numeros.sort(()=>{return Math.random()-0.5});
+//Write in console the numeros array of random way
+console.log(numeros);
+
